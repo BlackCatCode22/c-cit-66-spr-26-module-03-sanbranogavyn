bf834ecd7570f5cf9bd42232e953ab51113780b9
sanbranogavyn <sanbranogavyn@gmail.com>
2026-02-17 13:44:28 -0800

    Add files via upload

---
+++ arraysAreEasy/arraysAreEasy.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+    int nums[2][2][2] = {
+        { {1, 2}, {3, 4} },
+        { {5, 6}, {7, 8} }
+    };
+
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            for (int k = 0; k < 2; k++) {
+                cout << nums[i][j][k] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
