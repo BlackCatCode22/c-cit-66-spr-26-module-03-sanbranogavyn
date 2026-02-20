986c65722410c5446176440ac4f714da9f045709
sanbranogavyn <sanbranogavyn@gmail.com>
2026-02-20 15:57:17 -0800

    Add files via upload

---
+++ pointersArePowerful/pointersArePowerful.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int arr[5] = {10, 20, 30, 40, 50};
+    int *p = arr;
+
+    for (int i = 0; i < 5; i++) {
+        cout << (p + i) << endl;
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < 5; i++) {
+        cout << *p << endl;
+        p++;
+    }
+
+    return 0;
+}
